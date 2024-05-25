e4b5c788d6fe24603cd90dd47f2d135b916b5206
Kwasi  Addo Nyarko <kwasiaddonyarkojunior@gmail.com>
2024-05-25 21:31:34 +0000

    edit profile

--- app/(root)/profile/edit/page.tsx
+++ app/(root)/profile/edit/page.tsx
@@ -1,8 +1,27 @@
 import CustomBreadCrumbs from '@/components/CustomElements/CustomBreadCrumbs'
 import OnboardingForm from '@/components/CustomElements/OnboardingForm'
+import { fetchUser } from '@/lib/actions/user.actions';
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const EditProfile = () => {
+const EditProfile = async() => {
+  const user = await currentUser();
+  if (!user) return redirect('/');
+
+  const userInfo = await fetchUser(user.id);
+  const userData = {
+    clerkId: user.id,
+    firstName: userInfo?.firstName ?? user.firstName ?? "",
+    lastName: userInfo?.lastName ?? user.lastName ?? "",
+    image: userInfo?.profileImg ?? user.imageUrl,
+    city: userInfo?.city ?? "",
+    region: userInfo?.region ?? "",
+    country: userInfo?.country ?? "",
+    phoneNumber: userInfo?.phoneNumber ?? "",
+    isStudent: userInfo?.isStudent ?? false,
+    university: userInfo?.university ?? "",
+  };
   return (
     <section className="w-full flex flex-col">
         <section className="">
@@ -10,7 +29,7 @@ const EditProfile = () => {
         <h1 className='font-bold text-xl '>Edit profile</h1>
         </section>
         <section className="w-full flex justify-center">
-            <OnboardingForm/>
+            <OnboardingForm user={userData} />
         </section>
         
     </section>
